1bf017b54806584002cc440f50051408ae5770b5
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-11-03 23:51:51 -0400

    igual que el primer deploy pero utilizando pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
-import Tailwind from 'primevue/passthrough/tailwind';
+
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
@@ -12,7 +12,7 @@ const app = createApp(App)
 const pinia = createPinia()
 
 
-app.use(PrimeVue, { unstyled: true, pt: Tailwind });
+
 pinia.use(piniaPluginPersistedState)
 app.use(pinia)
 app.use(router)
