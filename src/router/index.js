e6917ee3602171b35bb64b4316bf4c5f8138e18f
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-11-04 20:31:44 -0400

    1.2

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import ShoppingCartView from '../views/ShoppingCartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  base: '/',
   routes: [
     {
       path: '/',
