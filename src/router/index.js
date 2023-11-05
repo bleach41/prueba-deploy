c1a50b31b7a28e49cbe676c16422ecbe177b069f
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-11-04 20:09:05 -0400

    corriengiendo routing 1.1

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ShoppingCartView from '../views/ShoppingCartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  base: '/',
   routes: [
     {
       path: '/',
